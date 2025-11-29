126b4dcf5f97822ebd26ff6fbd1eae27e548b89a
abeerfaiz <44645604+abeerfaiz@users.noreply.github.com>
2025-11-29 20:52:31 +0000

    Managing Circular References Using @JsonIgnore

--- src/main/java/com/social/media/models/SocialGroup.java
+++ src/main/java/com/social/media/models/SocialGroup.java
@@ -1,5 +1,6 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,6 +19,7 @@ public class SocialGroup {
     private Long id;
 
     @ManyToMany(mappedBy = "socialGroups")
+    @JsonIgnore
     private Set<SocialUser> socialUsers = new HashSet<>();
 
     @Override
--- src/main/java/com/social/media/models/SocialProfile.java
+++ src/main/java/com/social/media/models/SocialProfile.java
@@ -1,5 +1,6 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,6 +18,7 @@ public class SocialProfile {
 
     @OneToOne
     @JoinColumn(name = "social_user")
+    @JsonIgnore
     private SocialUser user;
 
     @Override
